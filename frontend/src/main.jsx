7380d83951475fdf9599be9686763b99f8e2af2f
raihan1338 <raihanraihan1819@gmail.com>
2026-06-01 06:59:01 +0700

    fix: add 50 games to seed, fix ReactDOM import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 import { startKeepAlive } from './utils/keepAlive.js'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
 
 startKeepAlive()
 
